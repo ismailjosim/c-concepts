cc1509ac9c3c5bab832745bcf514c01f4199d74c
ismail <ismailjosim99@gmail.com>
2023-10-16 00:36:37 +0600

    array accessing

---
+++ Arrays/array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int ar[5]; // create 10 var.
+
+    /* *** when we create an array it will create that particular item serially. for example if we create an array of int value. it will take 4 bytes for each array of index value.
+    suppose:
+    the 1st index is 400 then the nex will be 404
+    3rd => 408
+    4th => 412
+    5th => 416
+
+    it maintain serial for easy access of using index value.
+    */
+
+    // access array value
+
+    // 1.
+
+    return 0;
+}
