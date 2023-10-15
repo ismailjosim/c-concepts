2fafa0b1061e58183246a7c63ae26909f6b2fad4
ismail <ismailjosim99@gmail.com>
2023-10-15 21:09:13 +0600

    continue statement

--- loop/brakStatement.c
+++ loop/brakStatement.c
@@ -4,7 +4,12 @@ int main()
 {
     for (int i = 0; i <= 10; i++)
     {
-        if (i == 5)
+        if (i == 0)
+        {
+            continue;
+        }
+
+        if (i == 6)
         {
             break;
         }
