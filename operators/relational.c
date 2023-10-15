7036c57891753da4cb0787b542ba31d1e22abd15
ismail <ismailjosim99@gmail.com>
2023-10-15 20:32:30 +0600

    relational operatior

---
+++ operators/relational.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 20;
+    int b = 7;
+
+    /*
+    Less then <
+    greater then >
+    less then or equal to <=
+    greater then or equal to >=
+    equal to ==
+    not equal !=
+    */
+
+    return 0;
+}
