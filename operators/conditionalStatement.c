9fef1fb0174390b457778325c95685dc2d07d800
ismail <ismailjosim99@gmail.com>
2023-10-15 20:43:46 +0600

    if-else, else-if and nested if condition

---
+++ operators/conditionalStatement.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    if (a == b)
+    {
+        printf("Both Are Equal!");
+    }
+    else if (a > b)
+    {
+        printf("A is Higher then B");
+    }
+
+    else
+    {
+        printf("Both Are Not Equal!");
+    }
+    return 0;
+}
