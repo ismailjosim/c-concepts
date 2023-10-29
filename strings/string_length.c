835e97b942b79c46bf9e88bbe6031574aa590e29
ismail <ismailjosim99@gmail.com>
2023-10-29 15:11:14 +0600

    string with space & length of string

--- strings/string_length.c
+++ strings/string_length.c
@@ -3,8 +3,27 @@
 
 int main()
 {
-    char a[] = "Hello World!";
-    int length = strlen(a);
+    char b[] = "Hello World!";
+    int length = strlen(b);
     printf("The value of a is %d\n", length);
+
+    char a[100];
+    scanf("%s", a);
+    int count = 0;
+    // * 1. find length using while loop
+    int i = 0;
+    while (a[i] != '\0')
+    {
+        count++;
+        i++;
+    }
+    // * 1. find length using for loop
+
+    for (int i = 0; a[i] != '\0'; i++)
+    {
+        count++;
+    }
+    printf("%d\n", count);
+
     return 0;
 }
